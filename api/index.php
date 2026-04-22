cbc20727c620522204b908bbddde373f60f52635
islam-24-03 <isabhnisa@gmail.com>
2026-04-22 14:27:12 +0600

    Final Vercel structure: index moved to api, paths fixed

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once '../config.php';
 
 $page = $_GET['page'] ?? 'catalog';
 $is_admin = isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'];
@@ -9,30 +9,30 @@ if (str_starts_with($page, 'admin_') && !$is_admin && $page !== 'admin_login') {
     exit;
 }
 
-require_once 'views/header.php';
+require_once '../views/header.php';
 
 switch ($page) {
     case 'catalog':
-        require_once 'views/catalog.php';
+        require_once '../views/catalog.php';
         break;
     case 'admin_login':
-        require_once 'views/admin_login.php';
+        require_once '../views/admin_login.php';
         break;
     case 'admin_products':
-        require_once 'views/admin_products.php';
+        require_once '../views/admin_products.php';
         break;
     case 'admin_orders':
-        require_once 'views/admin_orders.php';
+        require_once '../views/admin_orders.php';
         break;
     case 'product':
-        require_once 'views/product.php';
+        require_once '../views/product.php';
         break;
     case 'cart':
-        require_once 'views/cart.php';
+        require_once '../views/cart.php';
         break;
     default:
-        require_once 'views/catalog.php';
+        require_once '../views/catalog.php';
         break;
 }
 
-require_once 'views/footer.php';
+require_once '../views/footer.php';
